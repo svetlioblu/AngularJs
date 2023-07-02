b5f6239b78d7037f6f014fd4e82960199f3a34a1
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-02 12:01:33 +0300

    add http module into the app.service

--- Forum/src/app/api.service.ts
+++ Forum/src/app/api.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-
+//import HttpClient module
+import { HttpClient } from '@angular/common/http';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  // add HttpClient in the constructor
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getThemes(){}
+  getPosts(){}
+
 }
