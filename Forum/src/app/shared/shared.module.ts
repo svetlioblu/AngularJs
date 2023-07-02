603089c4fa2267016f6c50e19f4f3dc437441bdb
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-02 22:03:48 +0300

    add loading

--- Forum/src/app/shared/shared.module.ts
+++ Forum/src/app/shared/shared.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
 
-
-
 @NgModule({
-  declarations: [
-    LoaderComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LoaderComponent],
+  imports: [CommonModule],
+  exports: [LoaderComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
