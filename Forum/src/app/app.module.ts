b6623ad8afd8fbf6e9319f48a873bf7a47e2853e
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-02 12:01:08 +0300

    import http module

--- Forum/src/app/app.module.ts
+++ Forum/src/app/app.module.ts
@@ -6,11 +6,18 @@ import { CoreModule } from './core/core.module';
 import { MainComponent } from './main/main.component';
 import { TeamsListComponent } from './teams-list/teams-list.component';
 import { PostsListComponent } from './posts-list/posts-list.component';
+// import http module
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, MainComponent, TeamsListComponent, PostsListComponent],
-  //Import the core module
-  imports: [BrowserModule, CoreModule],
+  declarations: [
+    AppComponent,
+    MainComponent,
+    TeamsListComponent,
+    PostsListComponent,
+  ],
+  //Import the core and http module
+  imports: [BrowserModule, CoreModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
