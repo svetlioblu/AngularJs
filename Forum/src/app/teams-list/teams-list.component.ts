d840bf5b1ddf3fc7cb71aa3872f7bffcbb67a7d1
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-02 12:35:42 +0300

    add themes in teams list

--- Forum/src/app/teams-list/teams-list.component.ts
+++ Forum/src/app/teams-list/teams-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-teams-list',
   templateUrl: './teams-list.component.html',
-  styleUrls: ['./teams-list.component.css']
+  styleUrls: ['./teams-list.component.css'],
 })
-export class TeamsListComponent {
-
+export class TeamsListComponent implements OnInit {
+  //Inject apiService
+  constructor(private apiService: ApiService) {}
+  ngOnInit(): void {
+    this.apiService.getThemes().subscribe((themes) => {
+      console.log(themes);
+    });
+  }
 }
