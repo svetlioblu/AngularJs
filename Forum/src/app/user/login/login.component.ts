1080a1c0e6ed1ee6039dedbb72dbf4074d8821c2
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-14 08:00:19 +0300

    add userservice in login.component.ts

--- Forum/src/app/user/login/login.component.ts
+++ Forum/src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(private userService: UserService) { }
 
   login(email: string, password: string): void {
-
+    //todo for now we are not handling th data
+    this.userService.login()
   }
 }
