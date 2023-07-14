305fb9462fa54a85ae091dbe64eb354766a16de3
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-14 07:17:01 +0300

    add route children

--- Forum/src/app/theme/theme-routing.module.ts
+++ Forum/src/app/theme/theme-routing.module.ts
@@ -2,18 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from '../main/main.component';
 import { NewThemeComponent } from './new-theme/new-theme.component';
+import { CurrentThemeComponent } from './current-theme/current-theme.component';
 
 
 
 const routes: Routes = [
-  { path: 'themes', component:MainComponent},
-  { path: 'add-theme', component:NewThemeComponent}
+    
+    {
+        path: 'themes',
+        children: [
+            { path: '', pathMatch: 'full', component: MainComponent },
+            { path: ':themeId', component: CurrentThemeComponent }
 
- 
+        ]
+    },
+    { path: 'add-theme', component: NewThemeComponent }
+
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class ThemeRoutingModule { }
