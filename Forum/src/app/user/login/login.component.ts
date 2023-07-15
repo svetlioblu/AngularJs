791567374fd8e962bc15e4c34bb3794ab4e3f0db
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-15 08:40:43 +0300

    add login navigate to home

--- Forum/src/app/user/login/login.component.ts
+++ Forum/src/app/user/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +8,11 @@ import { UserService } from '../user.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService,private router:Router) { }
 
   login(email: string, password: string): void {
     //todo for now we are not handling th data
     this.userService.login()
+    this.router.navigate(['/'])
   }
 }
