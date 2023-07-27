b3cf86218e86276471df09f4472b56f915bb06d8
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-27 07:49:36 +0300

    update User type

--- Forum/src/app/types/user.ts
+++ Forum/src/app/types/user.ts
@@ -1,7 +1,8 @@
 export interface User {
-    email: string,
-    firstName: string,
-    // phoneNumber: string,
-    // password: string,
-  
-  }
+  email: string,
+  firstName: string,
+  username: string,
+  tel: string
+  // phoneNumber: string,
+  // password: string,
+}
