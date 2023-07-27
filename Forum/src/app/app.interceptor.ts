b08f190bef4ed2bd6828e2b7b20186f9fd9ffa31
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-27 07:49:17 +0300

    update interceptor api replace

--- Forum/src/app/app.interceptor.ts
+++ Forum/src/app/app.interceptor.ts
@@ -2,16 +2,23 @@
 import { HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable, Provider } from "@angular/core";
 import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
 
+const { apiUrl } = environment
 
 @Injectable()
 //1
 export class AppInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-
-        return null
+        if (req.url.startsWith('/api')) {
+            req = req.clone({
+                url: req.url.replace('/api', apiUrl),
+                // will use all avalailable cookies on post
+                withCredentials: true
+            })
+        }
+        return next.handle(req)
     }
 }
 //2
@@ -19,4 +26,6 @@ export const AppInterceptorProvider: Provider = {
     multi: true,
     useClass: AppInterceptor,
     provide: HTTP_INTERCEPTORS
-} 
+}
+
+//3 add in app.module, providers
