ced14fe1d5a22284e41efb0cdfb30a8f707da4dc
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-28 13:57:04 +0300

    update user.service

--- Forum/src/app/user/user.service.ts
+++ Forum/src/app/user/user.service.ts
@@ -20,23 +20,13 @@ export class UserService {
   get isLogged(): boolean {
     return !!this.user
   }
-  constructor(private http: HttpClient) {
-    // try {
-    //   const lsUser = localStorage.getItem(this.USER_KEY) || ''
-    //   this.user = JSON.parse(lsUser)
-
-    // } catch (error) {
-    //   this.user = undefined
-    // }
-  }
+  constructor(private http: HttpClient) { }
 
   login(email: string, password: string) {
-    // this.user = {
-    //   email: 'mexikaneza@abv.bg',
-    //   firstName: 'John',
-    // }
-    // localStorage.setItem(this.USER_KEY, JSON.stringify(this.user))
-    return this.http.post<User>('/api/login', { email, password }).pipe(tap((user) => this.user$$.next(user)))
+
+    return this.http
+      .post<User>('/api/login', { email, password })
+      .pipe(tap((user) => this.user$$.next(user)))
   }
 
   register(username: string,
@@ -45,12 +35,15 @@ export class UserService {
     rePassword: string,
     tel: string) {
 
-    return this.http.post('/api/register', { username, email, password, rePassword, tel })
+    return this.http
+      .post<User>('/api/register', { username, email, password, rePassword, tel })
+      .pipe(tap((user) => this.user$$.next(user)))
   }
 
-  logout(): void {
-    this.user = undefined
-    localStorage.removeItem(this.USER_KEY)
-  }
+  logout() {
 
+    return this.http
+      .post<User>('/api/logout', {})
+      .pipe(tap(() => this.user$$.next(undefined)))
+  }
 }
