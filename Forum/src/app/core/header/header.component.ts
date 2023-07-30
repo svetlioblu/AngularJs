7c27fa83d7a27bf64b85d50d4eed612d173deb6c
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-30 19:46:36 +0300

    update header component routerLink

--- Forum/src/app/core/header/header.component.ts
+++ Forum/src/app/core/header/header.component.ts
@@ -20,10 +20,10 @@ export class HeaderComponent {
   logout(): void {
     this.userService.logout().subscribe({
       next: () => {
-        this.router.navigate(['/login'])
+        this.router.navigate(['/auth/login'])
       },
       error: () => {
-        this.router.navigate(['/login'])
+        this.router.navigate(['/auth/login'])
 
       }
     })
