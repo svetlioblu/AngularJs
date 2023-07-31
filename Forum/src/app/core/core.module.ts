ba0663c33021db7e24054a2f0a17b985eda1c2cf
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-31 08:14:49 +0300

    add error.component

--- Forum/src/app/core/core.module.ts
+++ Forum/src/app/core/core.module.ts
@@ -4,9 +4,10 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, NotFoundComponent],
+  declarations: [HeaderComponent, FooterComponent, NotFoundComponent, ErrorComponent],
   //add RouterModule
   imports: [CommonModule, RouterModule],
   //Which components to be exported from this module
