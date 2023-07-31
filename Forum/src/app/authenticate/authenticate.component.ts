4b8fff5b83bf858986c044b714432ba20739128d
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-31 08:14:02 +0300

    add authenticate component wrapper

--- Forum/src/app/authenticate/authenticate.component.ts
+++ Forum/src/app/authenticate/authenticate.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-authenticate',
   templateUrl: './authenticate.component.html',
   styleUrls: ['./authenticate.component.css']
 })
-export class AuthenticateComponent {
+export class AuthenticateComponent implements OnInit {
+  isAuthenticateing = true
 
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getProfile().subscribe({
+      next: () => {
+        this.isAuthenticateing = false
+      },
+      error: () => {
+        this.isAuthenticateing = false
+
+      },
+      complete: () => {
+        this.isAuthenticateing = false
+
+      }
+    })
+  }
 }
