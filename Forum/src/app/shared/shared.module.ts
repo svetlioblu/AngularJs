485e282eae92a32e8e1573dfeb1bacd018465003
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-31 18:29:37 +0300

    add slice pipe

--- Forum/src/app/shared/shared.module.ts
+++ Forum/src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
 import { AppEmailDirective } from './validators/app-email.directive';
+import { SlicePipe } from './pipes/slice.pipe';
 
 @NgModule({
-  declarations: [LoaderComponent, AppEmailDirective],
+  declarations: [LoaderComponent, AppEmailDirective, SlicePipe],
   imports: [CommonModule],
   exports: [LoaderComponent, AppEmailDirective],
 })
