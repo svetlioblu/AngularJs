0300d600a387f23bfba969be1dd85e5d8ce5f1be
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-07-31 18:27:23 +0300

    add catchError in app.interceptor

--- Forum/src/app/app.interceptor.ts
+++ Forum/src/app/app.interceptor.ts
@@ -1,14 +1,17 @@
 //create manually!
 import { HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable, Provider } from "@angular/core";
-import { Observable } from "rxjs";
+import { Router } from "@angular/router";
+import { Observable, catchError } from "rxjs";
 import { environment } from "src/environments/environment";
+import { ErrorService } from "./core/error/error.service";
 
 const { apiUrl } = environment
 
 @Injectable()
 //1
 export class AppInterceptor implements HttpInterceptor {
+    constructor(private router: Router, private errorService: ErrorService) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         if (req.url.startsWith('/api')) {
@@ -18,7 +21,17 @@ export class AppInterceptor implements HttpInterceptor {
                 withCredentials: true
             })
         }
-        return next.handle(req)
+        return next.handle(req).pipe(
+            catchError((err) => {
+                if (err.status == 401) {
+                    this.router.navigate(['/auth/login'])
+                } else {
+                    this.errorService.setError(err)
+                    this.router.navigate(['/error'])
+                }
+                return [err]
+            })
+        )
     }
 }
 //2
