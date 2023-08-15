d7b287fde86baf1f44c6efc93987fd1f5c3bf2fd
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-08-15 07:27:01 +0300

    add getProcedures in procedure.service

--- Facecare/src/app/procedure.service.ts
+++ Facecare/src/app/procedure.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProcedureService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProcedures() {
+    const { apiUrl } = environment
+    return this.http.get(`${apiUrl}/procedures`)
+  }
 }
