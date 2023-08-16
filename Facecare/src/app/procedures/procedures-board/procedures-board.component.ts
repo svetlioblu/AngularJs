c9b0201ca66aaf0103aac393a30da957c7215ce1
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-08-16 08:14:54 +0300

    subscribe to getProcedure in ProceduresBoard Component

--- Facecare/src/app/procedures/procedures-board/procedures-board.component.ts
+++ Facecare/src/app/procedures/procedures-board/procedures-board.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProcedureService } from 'src/app/procedure.service';
+import { Procedure } from 'src/app/types/Procedure';
 
 @Component({
   selector: 'app-procedures-board',
   templateUrl: './procedures-board.component.html',
   styleUrls: ['./procedures-board.component.css']
 })
-export class ProceduresBoardComponent {
+export class ProceduresBoardComponent implements OnInit {
+  proceduresList: Procedure[] = []
+
+  constructor(private procedureService: ProcedureService) { }
+  // to do isLoading, userservice
+  ngOnInit(): void {
+    this.procedureService.getProcedures().subscribe({
+      next: (procedures) => {
+        console.log(procedures);
+      },
+      error: (err) => {
+        console.error(`Error: ${err}`)
+      }
+    })
+  }
 
 }
