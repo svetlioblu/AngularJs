9a8b4a0ce86d3fa7f9d6de61e102ebd25f4d8bcd
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-08-16 08:14:06 +0300

    add Procedure type in procedure.service

--- Facecare/src/app/procedure.service.ts
+++ Facecare/src/app/procedure.service.ts
@@ -1,16 +1,20 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+
 import { environment } from 'src/environments/environment.development';
+import { Procedure } from './types/Procedure';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProcedureService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http:HttpClient) { }
 
   getProcedures() {
     const { apiUrl } = environment
-    return this.http.get(`${apiUrl}/procedures`)
+    return this.http.get<Procedure[]>(`${apiUrl}/procedures`)
   }
 }
