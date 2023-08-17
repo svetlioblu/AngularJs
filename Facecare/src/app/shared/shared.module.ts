0091830b78281247752c1b882be428571a7b1e25
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-08-17 07:44:41 +0300

    add slice pipe

--- Facecare/src/app/shared/shared.module.ts
+++ Facecare/src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from './spinner/spinner.component';
+import { SlicePipe } from './pipes/slice.pipe';
 
 
 
 @NgModule({
   declarations: [
-    SpinnerComponent
+    SpinnerComponent,
+    SlicePipe
   ],
   imports: [
     CommonModule
