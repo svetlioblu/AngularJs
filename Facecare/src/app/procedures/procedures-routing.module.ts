89e8bec5375bc6dce5ee8c8735d2bfe5532800ac
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-08-17 08:18:59 +0300

    add child routing for procedure-details

--- Facecare/src/app/procedures/procedures-routing.module.ts
+++ Facecare/src/app/procedures/procedures-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProceduresBoardComponent } from './procedures-board/procedures-board.component';
+import { ProcedureDetailsComponent } from './procedure-details/procedure-details.component';
 
 
 const routes: Routes = [
@@ -8,14 +9,14 @@ const routes: Routes = [
         path: 'procedures',
         children: [
             { path: '', pathMatch: 'full', component: ProceduresBoardComponent },
-            // { path: ':themeId', component: CurrentThemeComponent }
+            { path: ':procedureId', component: ProcedureDetailsComponent }
         ]
     }
-  
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
 export class ProceduresRoutingModule { }
