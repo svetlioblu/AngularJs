c850f0ac4b7fe5eb53030a4c89ce170e413093fd
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-08-18 12:45:30 +0300

    update procedure-details.ts

--- Facecare/src/app/procedures/procedure-details/procedure-details.component.ts
+++ Facecare/src/app/procedures/procedure-details/procedure-details.component.ts
@@ -1,22 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProcedureService } from 'src/app/procedure.service';
+import { Procedure } from 'src/app/types/procedure';
+
 
 @Component({
   selector: 'app-procedure-details',
   templateUrl: './procedure-details.component.html',
   styleUrls: ['./procedure-details.component.css']
 })
-export class ProcedureDetailsComponent implements OnInit{
+export class ProcedureDetailsComponent implements OnInit {
+
+  procedure: Procedure | undefined
+
   //todo inject private userService: UserService when ready
   constructor(
     private procedureService: ProcedureService,
     private activatedRoute: ActivatedRoute) { }
 
-ngOnInit(): void {
-  const id = this.activatedRoute.snapshot.params['procedureId']
+  ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.params['procedureId']
 
-  console.log(id);
-  
-}
+    this.procedureService.getDetails(id).subscribe({
+      next: (procedure) => {
+        this.procedure = procedure
+      },
+      error: (err) => {
+        console.error(`Error: ${err}`)
+      }
+    })
+  }
 }
