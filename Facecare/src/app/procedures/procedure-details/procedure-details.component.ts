eebb74672dfc9481a66fe65201f36d3287405c97
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-08-18 08:29:10 +0300

    add initial logic in procedure-details component

--- Facecare/src/app/procedures/procedure-details/procedure-details.component.ts
+++ Facecare/src/app/procedures/procedure-details/procedure-details.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProcedureService } from 'src/app/procedure.service';
 
 @Component({
   selector: 'app-procedure-details',
   templateUrl: './procedure-details.component.html',
   styleUrls: ['./procedure-details.component.css']
 })
-export class ProcedureDetailsComponent {
+export class ProcedureDetailsComponent implements OnInit{
+  //todo inject private userService: UserService when ready
+  constructor(
+    private procedureService: ProcedureService,
+    private activatedRoute: ActivatedRoute) { }
 
+ngOnInit(): void {
+  const id = this.activatedRoute.snapshot.params['procedureId']
+
+  console.log(id);
+  
+}
 }
