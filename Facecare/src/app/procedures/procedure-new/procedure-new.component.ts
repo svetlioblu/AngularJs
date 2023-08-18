7c3c46be030388867180b5bf036509fe7917dff5
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-08-18 17:47:32 +0300

    add newProcedureSubmitHandler

--- Facecare/src/app/procedures/procedure-new/procedure-new.component.ts
+++ Facecare/src/app/procedures/procedure-new/procedure-new.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-procedure-new',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class ProcedureNewComponent {
 
+  newProcedureSubmitHandler(newProcedureForm: NgForm) {
+    if (newProcedureForm.invalid) {
+      return
+    }
+
+
+  }
 }
