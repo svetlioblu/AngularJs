dc99153481654e5a10b27ae1e96aae7b8dc09a13
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-08-18 17:46:25 +0300

    add rpputing for Add btn

--- Facecare/src/app/procedures/procedures-routing.module.ts
+++ Facecare/src/app/procedures/procedures-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProceduresBoardComponent } from './procedures-board/procedures-board.component';
 import { ProcedureDetailsComponent } from './procedure-details/procedure-details.component';
+import { ProcedureNewComponent } from './procedure-new/procedure-new.component';
 
 
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
         path: 'procedures',
         children: [
             { path: '', pathMatch: 'full', component: ProceduresBoardComponent },
+            { path: 'add', component: ProcedureNewComponent },
             { path: ':procedureId', component: ProcedureDetailsComponent }
         ]
     }
